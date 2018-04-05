75ef64e8442f897b9b3117764be3fb2e45096c81
Sam Thomson <samt@samt.st>
2018-04-05 22:09:30 +0100

    test three green_or_red_indicators

--- src/getMarketStatus.ts
+++ src/getMarketStatus.ts
@@ -1,25 +1,25 @@
 import { getAllCryptoValues, CMCCurrencySnapshot } from 'get-crypto-fiat-values'
 
 export default async () => {
-    const aResult: CMCCurrencySnapshot[] = await getAllCryptoValues()
+    const aResult: CMCCurrencySnapshot[] | null = await getAllCryptoValues()
 
     let one_hour: number = 0
     let one_day: number = 0
     let one_week: number = 0
 
-    aResult.forEach((cmcCurrency: CMCCurrencySnapshot) => {
-        one_hour += (cmcCurrency.percent_change_1h < 0) ? 1: -1
-        one_day += (cmcCurrency.percent_change_24h < 0) ? 1: -1
-        one_week += (cmcCurrency.percent_change_7d < 0) ? 1: -1
-    })
-
-    console.log('one hour: ', one_hour)
-    console.log('one day: ', one_day)
-    console.log('one week: ', one_week)
+    if (aResult) {
+        aResult.forEach((cmcCurrency: CMCCurrencySnapshot) => {
+            one_hour += (cmcCurrency.percent_change_1h < 0) ? 1: -1
+            one_day += (cmcCurrency.percent_change_24h < 0) ? 1: -1
+            one_week += (cmcCurrency.percent_change_7d < 0) ? 1: -1
+        })
+    }
     
     return {
         GREEN_OR_RED: {
-            ONE_HOUR: (one_hour < 0) ? 'RED' : 'GREEN'
+            ONE_HOUR: (one_hour < 0) ? 'RED' : 'GREEN',
+            ONE_DAY: (one_day < 0) ? 'RED' : 'GREEN',
+            ONE_WEEK: (one_week < 0) ? 'RED' : 'GREEN'
         }
     }
 }
--- test/getMarketStatus.ts
+++ test/getMarketStatus.ts
@@ -1,13 +1,21 @@
 import { expect } from 'chai'
 import getMarketStatus from '../src/getMarketStatus'
 
-// test green or red indicator
-
+// test green or red indicators
 describe('getMarketStatus function test', () => {
     it('should return green or red status correctly', async () => {
         const mStatus = await getMarketStatus()
+        
         expect(mStatus.GREEN_OR_RED.ONE_HOUR).to.exist
         expect(mStatus.GREEN_OR_RED.ONE_HOUR).to.be.a('string')
         expect(mStatus.GREEN_OR_RED.ONE_HOUR).to.equal('GREEN')
+        
+        expect(mStatus.GREEN_OR_RED.ONE_DAY).to.exist
+        expect(mStatus.GREEN_OR_RED.ONE_DAY).to.be.a('string')
+        expect(mStatus.GREEN_OR_RED.ONE_DAY).to.equal('GREEN')
+        
+        expect(mStatus.GREEN_OR_RED.ONE_WEEK).to.exist
+        expect(mStatus.GREEN_OR_RED.ONE_WEEK).to.be.a('string')
+        expect(mStatus.GREEN_OR_RED.ONE_WEEK).to.equal('GREEN')
     })
 })
